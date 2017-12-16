296d24fd891734310e04d4d926745c9d9549250c
NicholasBallard <nicholasericballard@gmail.com>
2017-12-16 11:57:17 -0500

    build FIXML body with ElementTree and f-string syntax

--- parse_xml.py
+++ parse_xml.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-from xml.etree.ElementTree import Element, SubElement
+from xml.etree.ElementTree import Element, SubElement, dump
 from pprint import pprint
 
 account = '12345'
@@ -9,13 +9,21 @@ quantity = "2"
 body = \
 f"""<?xml version="1.0" encoding="UTF-8"?><FIXML xmlns="http://www.fixprotocol.org/FIXML-5-0-SP2"><Order TmInForce="0" Typ="1" Side="1" Acct="{account}"><Instrmt SecTyp="CS" Sym="{symbol}"/><OrdQty Qty="{quantity}"/></Order></FIXML>"""
 
-root = Element("root")
+params = {
+    "time_in_force": "0",
+    "trade_type": "1",
+    "side": "1",
+    "account": account,
+    "security_type": "CS",
+    "symbol": symbol,
+    "quantity": quantity,
+}
 
-SubElement(root, "one")
-SubElement(root, "two", first="cat", second="dog")
-SubElement(root, "three")
+xml_declaration = '<?xml version="1.0" encoding="UTF-8"?>'
 
-for node in root:
-    print(node)
-
-pprint(ET.tostring(root))
+declaration = Element(xml_declaration)
+root = Element("FIXML", {'xmlns': 'http://www.fixprotocol.org/FIXML-5-0-SP2'})
+order = SubElement(root, "Order", {"TmInForce": f'{params["time_in_force"]}', "Typ": f'{params["trade_type"]}', "Side": f'{params["side"]}', "Acct": f'{params["account"]}'})
+SubElement(order, "Instrmt", {"SecTyp": f'{params["security_type"]}', "Sym": f'{params["symbol"]}'})
+SubElement(order, "OrdQty", {"Qty": f'{params["quantity"]}'})
+dump(root)
